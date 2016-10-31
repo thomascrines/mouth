2bcf9515719edf446390c7e79a259c9b57c6f216
Thomas Crines <thomascrines@hotmail.com>
2016-10-31 16:03:58 +0000

    getName, toothCount, growTooth, fullCompliment all pass test

--- Mouth.java
+++ Mouth.java
@@ -6,4 +6,28 @@ public class Mouth{
     this.name = name;
     this.gums = new Tooth[32];
   }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int toothCount(){
+    int counter = 0;
+    for (Tooth tooth : gums) {
+      if (tooth != null){
+        counter ++;
+      }
+    }
+    return counter;
+  }
+
+  public void growTooth(Tooth tooth) {
+    int teeth = toothCount();
+    gums[teeth] = tooth;
+  }
+
+  public boolean fullCompliment(){
+    return toothCount() == gums.length;
+  }
+
 }
--- MouthTest.java
+++ MouthTest.java
@@ -13,6 +13,26 @@ public class MouthTest{
 
   @Test
   public void hasName(){
-    assertEquals("A Mouth", mouth.getName)
+    assertEquals("A Mouth", mouth.getName());
   }
+
+  @Test
+  public void noTeethAtStart(){
+    assertEquals(0, mouth.toothCount());
+  }
+
+  @Test
+  public void canGrowTeeth(){
+    mouth.growTooth(tooth);
+    assertEquals(1, mouth.toothCount());
+  }
+
+  @Test
+  public void cantGrowTooMany(){
+    for (int i = 0; i < 32 ; i++){
+      mouth.growTooth(tooth);
+    }
+    assertEquals(true, mouth.fullCompliment());
+  }
+
 }
